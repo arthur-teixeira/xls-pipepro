94827c3ecee5aa2af72290ada618bbbd9a867f47
arthur <arthurcarvalho24@gmail.com>
2019-11-06 17:22:42 -0300

    typos, commented currently unused code

--- helpers/errorHandler.js
+++ helpers/errorHandler.js
@@ -1,22 +1,11 @@
-const handler = (msg, status = 500) => {
-  const error = new Error(msg);
-  error.statusCode = status;
-  throw error;
-};
-
-
 module.exports = {
   //eslint-disable-next-line no-unused-vars
   middleware: (err, req, res, next) => {
-    res.status(500).send(err.toString())
+    res.status(err.status || 500).send(err.toString());
   },
-  notFoundError: msg => {
-    handler(msg, 404);
-  },
-  validationError: msg => {
-    handler(msg, 401);
-  },
-  internalServerError: msg => {
-    handler(msg, 500);
+  errCreator: (msg, status) => {
+    const err = new Error(msg);
+    err.status = status;
+    return err;
   }
 };
--- helpers/isAuth.js
+++ helpers/isAuth.js
@@ -1,12 +1,12 @@
 const jwt = require("jsonwebtoken");
-const errorHandler = require("./errorHandler").validationError;
+const createErr = require("./errorHandler").errCreator;
 
 module.exports = (req) => {
   const token = !!(req.body.token || req.query.token || req.headers["x-access-token"] || req.cookies.token);
 
-  if (!token) return validationError("Não autorizado: Token não fornecido");
+  if (!token) return createErr("Não autorizado: Token não fornecido", 401);
   jwt.verify(token, process.env.SECRET, (err, decoded) => {
-    if (err) return validationError("Não autorizado: Token inválido");
+    if (err) return createErr("Não autorizado: Token inválido", 401);
     req.email = decoded.email;
   });
 };
